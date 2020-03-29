80625f4c27c78efc0f370993c82fc0097730d780
Victor Cataño <vdcatanoc@unal.edu.co>
2020-03-29 17:32:05 -0500

    added multipage crawl

--- planetadelibros/spiders/spider.py
+++ planetadelibros/spiders/spider.py
@@ -3,14 +3,20 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from planetadelibros.items import BookItem
 
+
 # NC: Novela contemporanea
 class PlanetaCrawlerNC(scrapy.Spider):
     name = "crawlerPlaneta"
     start_urls = [
-        'https://www.planetadelibros.com.co/libros/novelas/00038'
-
-                  ]
-    i = 1
+        'https://www.planetadelibros.com.co/libros/novelas/00038',
+        'https://www.planetadelibros.com.co/libros/novela-historica/00013',
+        'https://www.planetadelibros.com.co/libros/novela-literaria/00012',
+        'https://www.planetadelibros.com.co/libros/novela-negra/00015',
+        'https://www.planetadelibros.com.co/libros/novelas-romanticas/00014',
+        'https://www.planetadelibros.com.co/libros/poesia/00051',
+        'https://www.planetadelibros.com.co/libros/teatro/00052'
+    ]
+    npag = 0
 
     def parse(self, response):
         for libro in response.xpath('//ul[@class="llibres-miniatures llibres-graella"]/li'):
@@ -19,28 +25,25 @@ class PlanetaCrawlerNC(scrapy.Spider):
                 detalle_libro = libro.xpath('div[@class="titol"]/span/@data-link-js').get()
                 request = scrapy.Request(detalle_libro, callback=self.parse_libro)
                 yield request
-           
 
-
-        '''next_page = response.xpath('//div[@class="paginacio-seguent"]/a/@href').extract()
-        if next_page is not None:
-            print("SIGUIENTE ", next_page[0])
-            next_page = response.urljoin(next_page[0])
+        next_page = response.xpath('//div[@class="paginacio-seguent"]/a/@href').get()
+        if next_page is not None and self.npag < int(self.maxpag):
+            print("SIGUIENTE ", next_page)
+            next_page = response.urljoin(next_page)
             #print(next_page)
             #print("********PAGINA ", self.i)
-            self.i = self.i + 1
-            yield scrapy.Request(str(next_page), callback=self.parse, dont_filter=True)'''
+            self.npag += 1
+            yield scrapy.Request(next_page, callback=self.parse, dont_filter=True)
 
-   
     def parse_libro(self, response):
         item = BookItem()
         nombre = response.css('div.titol::text').get()
-        autor = response.css('div.autors h2::text').get()
+        autor = response.css('div.autors h2 a::text').get()
         if autor is None:
-            autor = response.css('div.autors h2 a::text').get()
+            autor = response.css('div.autors h2::text').get()
         editorial = response.css('div.segell-nom a::text').get()
         nro_paginas = response.css('#num_pagines::text').get()[19:]
-        precio = response.css('div.preu::text').get()[2:].replace(".","")
+        precio = response.css('div.preu::text').get()[2:].replace(".", "")
 
         item['nombre'] = nombre
         item['autor'] = autor
--- planetadelibros/spiders/spider2.py
+++ planetadelibros/spiders/spider2.py
@@ -1,34 +1,70 @@
 import scrapy
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from planetadelibros.items import BookItem
 
+
 # NC: Novela contemporanea
 class PanaCrawlerNC(CrawlSpider):
     name = "crawlerPana"
-    start_urls = ['https://www.panamericana.com.co/buscapagina?fq=C:/1/15/73/&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=1'] #ciencia ficcion
-    allowed_domains = ['panamericana.com.co']
-    i = 1
+    urls_base = [
+        # ciencia ficcion
+        'https://www.panamericana.com.co/buscapagina?fq=C:/1/15/73/&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+
+        # cuento
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f74%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # critica
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f20%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # hispanoamericana
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f17%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # colombiana
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f18%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # teatro
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f22%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # ficcion
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f75%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # poesia
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f21%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber=',
+        # historica
+        'https://www.panamericana.com.co/buscapagina?fq=C%3a%2f1%2f15%2f19%2f&PS=12&sl=5e2a6963-80df-4508-b796-cb6a21d8fa05&cc=1&sm=0&PageNumber='
+
+    ]
+    # allowed_domains = ['panamericana.com.co']
+
+    def start_requests(self):
+        for url in self.urls_base:
+            yield scrapy.Request(url + '1', meta={"urlbase": url, "page": 1})
 
     def parse(self, response):
-        for libro in response.css('h3.item__showcase__category__title'):
-            detalle_libro = libro.css('a::attr(href)').get()
-            request = scrapy.Request(detalle_libro, callback=self.parse_libro, dont_filter=True)
+        page = response.meta.get('page')
+        if len(response.text.strip()) != 0 and page <= int(self.maxpag): #para cuando llega a una pagina vacia o al maximo
+
+            for libro in response.css('h3.item__showcase__category__title'):
+                detalle_libro = libro.css('a::attr(href)').get()
+                request = scrapy.Request(detalle_libro, callback=self.parse_libro, dont_filter=True)
+                yield request
+
+            #otras paginas
+            request = scrapy.Request(response.meta.get("urlbase") + str(page+1))
+            request.meta["page"] = page+1
+            request.meta["urlbase"] = response.meta.get("urlbase")
             yield request
-           
-   
+
     def parse_libro(self, response):
         item = BookItem()
         nombre = response.css("div.productName::text").get()
         autor = response.css('td.Autor-es-::text').get()
         editorial = response.css('div.productP__infoCont__shortDecription > div > a::text').get().strip()
         nro_paginas = response.css('td.N-°-paginas::text').get()
-        precio = response.css("#___rc-p-dv-id::attr(value)").get()
+        precio = float(response.css("#___rc-p-dv-id::attr(value)").get())
+        if precio == 9999876: precio = None
+
 
         item['nombre'] = nombre
         item['autor'] = autor
         item['editorial'] = editorial
         item['nro_paginas'] = int(nro_paginas)
-        item['precio'] = float(precio)
+        item['precio'] = precio
         item['url'] = response.url
         return item

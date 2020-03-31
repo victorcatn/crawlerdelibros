c66f9e13e1efadc287c3353b2deda96323e7493e
Victor Cataño <vdcatanoc@unal.edu.co>
2020-03-31 00:52:24 -0500

    executable change

--- crawl.bat
+++ crawl.bat
@@ -1,9 +1,2 @@
-@echo off
-if "%1" == "" (
-	echo agregue el numero maximo de paginas despues del comando, por ejemplo para dos paginas es: crawl.bat 2
-	exit)
-
-echo realizando el scraping, espere un momento
-
-python -m scrapy crawl crawlerPlaneta -a maxpag=%1
-python -m scrapy crawl crawlerPana -a maxpag=%1
+python -m scrapy crawl crawlerPlaneta -a maxpag=1
+python -m scrapy crawl crawlerPana -a maxpag=1
